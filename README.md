dbfcfe3124a7ce978fe860c5db911f452535181a
Iron512 <ivanmartini96@gmail.com>
2021-05-26 12:10:24 +0000

    README updated

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # LCSE project
-This repository is the final project for the Laboratory of Computer Science Education (LCSE) course, year 2020/2021. Feel free to use this material as you wish (as we just did reinvent the wheel).
+This repository is the final project for the Laboratory of Computer Science Education (LCSE) course, year 2020/2021. Feel free to use this material as you wish (we just did reinvent the wheel).
 
 ## Members
 Ivan Martini - Matr. 207597
 
 Giulia Peserico - Matr. ######
 
-## Usage
-TBD
+## Prerequisites
+- A terminal
+- Python3 
+
+## Setup
+This operations should be executed before the lab date. We are positive that these steps are enough on archlinux, but we expect those to be perfectly fine on other \*nix like OS. *By now* we have no idea on how they work on Windows, and they could require some tinkering.
+
+First, we need to setup the environment. To avoid misalignment on libraries or whatever, let's create and enable a virtual environment
+
+```bash
+$ Python3 -m venv virtual
+$ . virtual/bin/activate
+```
+
+Now proceed installing the libraries.
+```bash
+$ pip install -r requirements.txt
+```
+
--- tester.py
+++ tester.py
@@ -3,34 +3,65 @@
 import os
 import curses
 import time
+import numpy as np
+
+import helper
 
 def main(stdscr):
-	rows, columns = os.popen('stty size', 'r').read().split()
-	rows = int(rows)
-	columns = int(columns)
+	rows, columns = helper.screen_size()
 
+	#Disable cursor and blocking keyboard
 	stdscr.nodelay(1)
 	curses.curs_set(0)
 
+	#Initialize some colors
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+	curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+	curses.init_pair(5, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+	#Top left corner
 	stdscr.addstr(0,0,"\u2554")
 	stdscr.addstr(0,1,"\u2550")
 	stdscr.addstr(0,2,"\u2550")
 	stdscr.addstr(1,0,"\u2551")
 
+	#Bottom left corner
 	stdscr.addstr(0,columns-1,"\u2557")
 	stdscr.addstr(0,columns-2,"\u2550")
 	stdscr.addstr(0,columns-3,"\u2550")
 	stdscr.addstr(1,columns-1,"\u2551")
 
+	#Top right corner
 	stdscr.addstr(rows-1,0,"\u255A")
 	stdscr.addstr(rows-1,1,"\u2550")
 	stdscr.addstr(rows-1,2,"\u2550")
 	stdscr.addstr(rows-2,0,"\u2551")
 
-	stdscr.insstr(rows-1,columns-1,"\u255D")
+	#Bottom right corner
+	stdscr.insstr(rows-1,columns-1,"\u255D") #Avoid cursor to exceed window
 	stdscr.addstr(rows-1,columns-2,"\u2550")
 	stdscr.addstr(rows-1,columns-3,"\u2550")
 	stdscr.addstr(rows-2,columns-1,"\u2551")
+
+	#Some cool ascii writings
+	center = int(columns/2)
+	stdscr.addstr(10,center-25," ██████╗██╗   ██╗██████╗ ███████╗███████╗███████╗",curses.color_pair(5) | curses.A_BOLD)
+	stdscr.addstr(11,center-25,"██╔════╝██║   ██║██╔══██╗██╔════╝██╔════╝██╔════╝",curses.color_pair(5) | curses.A_BOLD)
+	stdscr.addstr(12,center-25,"██║     ██║   ██║██████╔╝███████╗█████╗  ███████╗",curses.color_pair(5) | curses.A_BOLD)
+	stdscr.addstr(13,center-25,"██║     ██║   ██║██╔══██╗╚════██║██╔══╝  ╚════██║",curses.color_pair(5) | curses.A_BOLD)
+	stdscr.addstr(14,center-25,"╚██████╗╚██████╔╝██║  ██║███████║███████╗███████║",curses.color_pair(5) | curses.A_BOLD)
+	stdscr.addstr(15,center-25," ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝",curses.color_pair(5) | curses.A_BOLD)
+
+	stdscr.addstr(rows-15,center-17,"████████╗███████╗███████╗████████╗",curses.color_pair(4) | curses.A_BOLD)
+	stdscr.addstr(rows-14,center-17,"╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝",curses.color_pair(4) | curses.A_BOLD)
+	stdscr.addstr(rows-13,center-17,"   ██║   █████╗  ███████╗   ██║   ",curses.color_pair(4) | curses.A_BOLD)
+	stdscr.addstr(rows-12,center-17,"   ██║   ██╔══╝  ╚════██║   ██║   ",curses.color_pair(4) | curses.A_BOLD)
+	stdscr.addstr(rows-11,center-17,"   ██║   ███████╗███████║   ██║   ",curses.color_pair(4) | curses.A_BOLD)
+	stdscr.addstr(rows-10,center-17,"   ╚═╝   ╚══════╝╚══════╝   ╚═╝   ",curses.color_pair(4) | curses.A_BOLD)
+
 	stdscr.refresh()
 
 	stay = True
@@ -49,10 +80,10 @@ def main(stdscr):
 			stay = False
 
 		item = cycle[count%len(cycle)]
-		stdscr.addstr(int(rows/2)+item[1],int(columns/2)+item[2],item[0])
+		stdscr.addstr(int(rows/2)+item[1],center+item[2],item[0],curses.color_pair(count%3+1))
 		stdscr.refresh()
-		stdscr.addstr(int(rows/2)+item[1],int(columns/2)+item[2]," ")
+		stdscr.addstr(int(rows/2)+item[1],center+item[2]," ")
 		count = count + 1
-		time.sleep(0.1)
+		time.sleep(0.15)
 
 curses.wrapper(main)
